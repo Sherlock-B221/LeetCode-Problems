33763a02ff5c6536712ee5d52cccabd0d4816f3a
Sherlock-B221 <e19cse246@bennett.edu.in>
2021-09-20 16:03:03 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (9.84%) - LeetHub

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
+    unordered_map<int,int> dp;
     int fib(int n) {
-        if ( n < 2 )
-            return n;
-        else
-        {
-            int a = 0, b = 1, sum;
-            for (int i = 2; i < n; i++ ) {
-                sum = a + b;
-                a = b;
-                b = sum;
-            }
-            return a+b;
-            
+        
+        if ( n < 2) return n;
+        
+        if ( dp[n] != 0 ) {
+            return dp[n];
         }
         
+        return dp[n] = fib(n-1) + fib(n-2); 
     }
 };
