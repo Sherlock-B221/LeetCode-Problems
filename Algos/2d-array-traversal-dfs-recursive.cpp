445a755a7c94750670c732edb6dd0e1063ec838d
Sherlock-B221 <e19cse246@bennett.edu.in>
2021-10-22 08:38:23 +0530

    Update 2d-array-traversal-dfs-recursive.cpp

--- Algos/2d-array-traversal-dfs-recursive.cpp
+++ Algos/2d-array-traversal-dfs-recursive.cpp
@@ -5,14 +5,14 @@ using namespace std;
 vector<vector<int>> directions = {{-1,0},{0,1},{1,0},{0,-1}};
 
 
-void traversalDFS(int row, int col, vector<vector<int>> matrix, vector<vector<bool>> &seen, vector<int> &vis)
+void traversalDFS(int row, int col, vector<vector<int>> matrix, vector<vector<bool>> &seen, vector<int> &val)
 {
   if(row<0 || col<0 || row>=matrix.size() || col>=matrix[0].size() || seen[row][col])
     return;
-  vis.push_back(matrix[row][col]);
+  val.push_back(matrix[row][col]);
   seen[row][col] = true;
   for (int i = 0; i < directions.size(); i++) {
-    traversalDFS(row + directions[i][0], col+directions[i][1], matrix, seen, vis);
+    traversalDFS(row + directions[i][0], col+directions[i][1], matrix, seen, val);
   }
 }
 
@@ -26,8 +26,8 @@ int main()
     {13,14,15,16}
   };
   vector<vector<bool>> seen(row,vector<bool>(col,false));
-  vector<int> vis;
-  traversalDFS(0,0, matrix, seen, vis);
+  vector<int> val;
+  traversalDFS(0,0, matrix, seen, val);
   for (auto i:vis) {
     cout<<i<<" ";
   }
