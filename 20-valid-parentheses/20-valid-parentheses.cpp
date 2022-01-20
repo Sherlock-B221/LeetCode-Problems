ea9d3d88340d9dfa21a706d39bbbfc571281afef
Sherlock-B221 <e19cse246@bennett.edu.in>
2022-01-21 02:05:27 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (50.05%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if ( s.length() == 0 ) return true;
+                int n=s.length();
+
+        if ( n == 0 ) return true;
         stack<char> stack;
-        for ( int i = 0; i< s.length(); i++) {
+        for ( int i = 0; i<n; i++) {
             if (s[i] == '[' || s[i] == '{' || s[i] == '(') {
                 stack.push(s[i]);
                 
