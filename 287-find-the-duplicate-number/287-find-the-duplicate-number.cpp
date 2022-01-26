a499b10744ebb09901adbfc2e51d52eeccc643d5
Sherlock-B221 <e19cse246@bennett.edu.in>
2022-01-26 22:47:41 +0530

    Time: 184 ms (35.24%), Space: 83.9 MB (14.36%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int n = nums.size() - 1;
-        int s = 0;
         unordered_map<int,int> m;
         for ( auto e : nums ) {
             if ( m[e] > 0 ) {
