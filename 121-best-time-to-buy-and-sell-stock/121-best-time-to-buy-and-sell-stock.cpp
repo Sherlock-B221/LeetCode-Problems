10d73b76896b2ad4b13b00e23fb171058613478e
Sherlock-B221 <e19cse246@bennett.edu.in>
2022-02-01 23:08:49 +0530

    Time: 157 ms (51.25%), Space: 93.4 MB (12.96%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -4,13 +4,12 @@ public:
         cin.tie(NULL);
         cout.tie(NULL);}
     int maxProfit(vector<int>& prices) {
-        int m = INT_MAX;
-        int maxProfit = 0;
-        for ( auto e : prices ) {
-            m = min(e,m);
-            maxProfit = max( e - m, maxProfit );
-            
+         int buy=prices[0];
+        int mp=0;
+        for(int k=1;k<prices.size();k++){
+            buy=min(buy,prices[k]);
+            mp=max(mp,prices[k]-buy);
         }
-        return maxProfit;
+        return mp;
     }
 };
