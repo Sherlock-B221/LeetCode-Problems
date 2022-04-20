8259335cad64eaf46e8cdb8123c9cd75e45965e8
Sherlock-B221 <e19cse246@bennett.edu.in>
2022-04-21 03:03:30 +0530

    Added solution - LeetHub

---
+++ 14. Lowest Common Ancestor in a Binary Tree - GFG/14.-lowest-common-ancestor-in-a-binary-tree.cpp
@@ -0,0 +1,188 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+    Node(){
+        data = 0;
+        left = right = NULL;
+    }
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+ // } Driver Code Ends
+/* A binary tree node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+class Solution
+{ 
+    public:
+    
+   bool findPath(Node *root, vector<Node*> &path, int k)
+{
+    // base case
+    if (root == NULL) return false;
+ 
+    // Store this node in path vector. The node will be removed if
+    // not in path from root to k
+    path.push_back(root);
+ 
+    // See if the k is same as root's key
+    if (root->data == k)
+        return true;
+ 
+    // Check if k is found in left or right sub-tree
+    if ( (root->left && findPath(root->left, path, k)) ||
+         (root->right && findPath(root->right, path, k)) )
+        return true;
+ 
+    // If not present in subtree rooted with root, remove root from
+    // path[] and return false
+    path.pop_back();
+    return false;
+}
+    Node* lca(Node* root ,int n1 ,int n2 )
+    {
+       vector<Node*> path1, path2;
+ 
+    // Find paths from root to n1 and root to n1. If either n1 or n2
+    // is not present, return -1
+    if ( !findPath(root, path1, n1) || !findPath(root, path2, n2))
+          return NULL;
+ 
+    /* Compare the paths to get the first different value */
+    int i;
+    for (i = 0; i < path1.size() && i < path2.size() ; i++)
+        if (path1[i] != path2[i])
+            break;
+    return path1[i-1];
+    }
+};
+
+// { Driver Code Starts.
+
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // for(string i:ip)
+    //     cout<<i<<" ";
+    // cout<<endl;
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// Function for Inorder Traversal
+void printInorder(Node* root)
+{
+    if(!root)
+        return;
+
+    printInorder(root->left);
+    cout<<root->data<<" ";
+    printInorder(root->right);
+}
+
+int main() {
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a,b;
+        scanf("%d %d ",&a,&b);
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+        Solution ob;
+        cout<<ob.lca(root,a,b)->data<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
